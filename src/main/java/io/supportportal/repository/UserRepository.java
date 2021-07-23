23f8e27a7f6c57fecb110dc543fc000de4538e07
dave dage <dage459@gmail.com>
2021-07-23 14:17:04 +0200

    Ass UserRepository

---
+++ src/main/java/io/supportportal/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package io.supportportal.repository;
+
+import io.supportportal.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    User findUserByUsername(String username);
+
+    User findUserByEmail(String email);
+}
